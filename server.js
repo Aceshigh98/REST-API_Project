0aba64bfa8cd5751bef40cc6ba7266931c7872ff
AcesHigh <cothermanbrannon@gmail.com>
2024-02-13 19:40:35 -0600

    Added .env npm

--- server.js
+++ server.js
@@ -1,16 +1,21 @@
 // Main server side file.
 
+import dotenv from 'dotenv';
+dotenv.config();
+
 // Import necessary modules
 import express from "express";
 import cors from "cors";
 import { store } from "./store/store.js";
 
+const apikey = process.env.API_KEY;
+
 const app = express();
-const port = 3000;
+const port = 80;
 
 app.use(cors());
 
-app.get("/BTC", async (req, res) => {
+app.get(apikey, async (req, res) => {
   try {
     // Call the store function and wait for the result
 
